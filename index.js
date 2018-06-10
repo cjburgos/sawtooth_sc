5abb497c3592dd31372ba0910f80a19445abd29b
cjburgos <30358158+cjburgos@users.noreply.github.com>
2018-06-10 17:52:11 -0400

    Removing unnecessary files

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-/**
- * Copyright 2016, 2017 Intel Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ------------------------------------------------------------------------------
- */
+const { TransactionProcessor } = require('sawtooth-sdk/processor')
 
-'use strict'
+const MMHandler = require('./mm_handler')
 
-module.exports = {
-  processor: require('./processor'),
-  exceptions: require('./processor/exceptions'),
-  protobuf: require('./protobuf'),
-  signing: require('./signing')
-}
+const transactionProcessor = new TransactionProcessor('127.0.0.1:4004')
+
+transactionProcessor.addHandler(new MMHandler())
+transactionProcessor.start()
+
+console.log(`Starting Material Transaction Processor`)
+console.log('Connecting to Sawtooth validator at 127.0.0.1:4004')
