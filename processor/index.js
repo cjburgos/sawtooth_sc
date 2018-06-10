59dd294b27090bf759a1f95e74d5a3d593443860
cjburgos <30358158+cjburgos@users.noreply.github.com>
2018-06-10 18:11:24 -0400

    Changes to index.js

--- processor/index.js
+++ processor/index.js
@@ -2,10 +2,10 @@ const { TransactionProcessor } = require('sawtooth-sdk/processor')
 
 const MMHandler = require('./mm_handler')
 
-const transactionProcessor = new TransactionProcessor('127.0.0.1:4004')
+const transactionProcessor = new TransactionProcessor('tcp://127.0.0.1:4004')
 
 transactionProcessor.addHandler(new MMHandler())
 transactionProcessor.start()
 
 console.log(`Starting Material Transaction Processor`)
-console.log('Connecting to Sawtooth validator at 127.0.0.1:4004')
+console.log('Connecting to Sawtooth validator at tcp://127.0.0.1:4004')
